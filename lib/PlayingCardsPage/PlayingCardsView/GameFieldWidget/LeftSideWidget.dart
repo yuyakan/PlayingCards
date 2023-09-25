a816652d4090a19899ed46aed1660231292ba798
yuyakan <kanbe1365@icloud.com>
2023-09-26 02:07:20 +0900

    feat: viewModelを追加した.

--- lib/PlayingCardsPage/PlayingCardsView/GameFieldWidget/LeftSideWidget.dart
+++ lib/PlayingCardsPage/PlayingCardsView/GameFieldWidget/LeftSideWidget.dart
@@ -1,37 +1,29 @@
 import 'package:flutter/material.dart';
 import 'package:high_and_low/Ad/ShowAdProvider/ShowAdByBackProvider.dart';
 import 'package:high_and_low/Ad/ShowAdProvider/ShowAdByResetProvider.dart';
-import 'package:high_and_low/PlayingCardsPage/Provider/CardsDeckProvider.dart';
 import 'package:high_and_low/PlayingCardsPage/Provider/FieldCardsProvider.dart';
 import 'package:high_and_low/PlayingCardsPage/Provider/GameStateProvider.dart';
-import 'package:high_and_low/PlayingCardsPage/Provider/TimesOfBackProvider.dart';
-import 'package:high_and_low/constants.dart';
+import 'package:high_and_low/PlayingCardsPage/ViewModel/GameViewModel.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class LeftSideWidget extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final _gameViewModel = GameViewModel(ref);
+    final Size size = MediaQuery.of(context).size;
+
+    final _ = ref.watch(fieldCardsProvider);
     final _isVisibleBackButtonState = ref.watch(isVisibleBackButtonProvider);
-    final _fieldCardsState = ref.watch(fieldCardsProvider);
     final _showAdByResetNotifier = ref.watch(showAdByResetProvider.notifier);
     final _showAdByBackNotifier = ref.watch(showAdByBackProvider.notifier);
-    final Size size = MediaQuery.of(context).size;
+
     return SizedBox(
       width: size.width * 0.2,
       child:
           Column(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
         MaterialButton(
           onPressed: () {
-            ref.read(timesOfBackProvider.notifier).reset();
-            ref.read(fieldCardsProvider.notifier).reset();
-            ref.read(cardsDeckProvider.notifier).reset();
-            ref.read(isVisibleBackButtonProvider.notifier).state = false;
-            ref.read(isVisibleOpenButtonProvider.notifier).state = true;
-            ref
-                .read(cardsDeckProvider.notifier)
-                .insertJoker(ref.read(isUsedJokerProvider.notifier).state);
-            ref.read(cardImageProvider.notifier).state = EMPTY_CARD;
-
+            _gameViewModel.reset();
             _showAdByResetNotifier.showAdByReset();
           },
           child: Text("Reset", style: TextStyle(fontSize: size.width * 0.03)),
@@ -47,17 +39,7 @@ class LeftSideWidget extends HookConsumerWidget {
             maintainAnimation: true,
             child: MaterialButton(
               onPressed: () {
-                ref.read(timesOfBackProvider.notifier).add();
-                ref.read(cardsDeckProvider.notifier).stackOnTop(
-                    ref.read(fieldCardsProvider.notifier).removeTop());
-                ref.read(cardImageProvider.notifier).state =
-                    ref.read(fieldCardsProvider.notifier).topCard();
-
-                ref.read(isVisibleBackButtonProvider.notifier).state =
-                    !_fieldCardsState.isEmpty;
-                ref.read(isVisibleOpenButtonProvider.notifier).state =
-                    !ref.read(cardsDeckProvider).isEmpty;
-
+                _gameViewModel.back();
                 _showAdByBackNotifier.showAdByBack();
               },
               child:
--- lib/PlayingCardsPage/PlayingCardsView/GameFieldWidget/RightSideWidget.dart
+++ lib/PlayingCardsPage/PlayingCardsView/GameFieldWidget/RightSideWidget.dart
@@ -1,20 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:high_and_low/Ad/ShowAdProvider/ShowAdByResetProvider.dart';
-import 'package:high_and_low/PlayingCardsPage/Provider/CardsDeckProvider.dart';
-import 'package:high_and_low/PlayingCardsPage/Provider/FieldCardsProvider.dart';
 import 'package:high_and_low/PlayingCardsPage/Provider/GameStateProvider.dart';
 import 'package:high_and_low/PlayingCardsPage/Provider/TimesOfBackProvider.dart';
-import 'package:high_and_low/constants.dart';
+import 'package:high_and_low/PlayingCardsPage/ViewModel/GameViewModel.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class RightSideWidget extends HookConsumerWidget {
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    final _gameViewModel = GameViewModel(ref);
+    final Size size = MediaQuery.of(context).size;
+
     final _isUsedJokerState = ref.watch(isUsedJokerProvider);
     final _timesOfBackState = ref.watch(timesOfBackProvider);
     final _isVisibleOpenButtonState = ref.watch(isVisibleOpenButtonProvider);
     final _showAdByResetNotifier = ref.watch(showAdByResetProvider.notifier);
-    final Size size = MediaQuery.of(context).size;
     return SizedBox(
       width: size.width * 0.2,
       child:
@@ -30,14 +30,8 @@ class RightSideWidget extends HookConsumerWidget {
             Switch(
               value: _isUsedJokerState,
               onChanged: (e) {
-                ref.read(isUsedJokerProvider.notifier).state = e;
-                ref.read(timesOfBackProvider.notifier).reset();
-                ref.read(fieldCardsProvider.notifier).reset();
-                ref.read(cardsDeckProvider.notifier).reset();
-                ref.read(isVisibleBackButtonProvider.notifier).state = false;
-                ref.read(isVisibleOpenButtonProvider.notifier).state = true;
-                ref.read(cardsDeckProvider.notifier).insertJoker(e);
-                ref.read(cardImageProvider.notifier).state = EMPTY_CARD;
+                _gameViewModel.insertJoker(e);
+                _gameViewModel.reset();
                 _showAdByResetNotifier.showAdByReset();
               },
             ),
@@ -50,17 +44,7 @@ class RightSideWidget extends HookConsumerWidget {
             maintainAnimation: true,
             child: MaterialButton(
               onPressed: () {
-                ref.read(timesOfBackProvider.notifier).subtract();
-                ref
-                    .read(fieldCardsProvider.notifier)
-                    .add(ref.read(cardsDeckProvider.notifier).drow());
-                ref.read(cardImageProvider.notifier).state =
-                    ref.read(fieldCardsProvider.notifier).topCard();
-
-                ref.read(isVisibleOpenButtonProvider.notifier).state =
-                    !ref.read(cardsDeckProvider).isEmpty;
-                ref.read(isVisibleBackButtonProvider.notifier).state =
-                    !ref.read(fieldCardsProvider).isEmpty;
+                _gameViewModel.flip();
               },
               child: Text(_timesOfBackState == 0 ? "Open" : "Next",
                   style: TextStyle(fontSize: size.width * 0.03)),
